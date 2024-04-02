4dc78f62df5d6ba188798cfe77435b205f788990
PaulBueckhard <paul.bueckhard@code.berlin>
2024-04-02 19:22:05 +0200

    added filename sanitizer

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,8 +1,12 @@
 from pytube import YouTube
 import os
+import re
 
-FORMAT = "mp4"  # mp4 for video, mp3 for audio
+FORMAT = "mp4"  # mp4 or mp3
 VIDEOS = [""]
+
+def sanitize_filename(filename):
+    return re.sub(r'[<>:"/\\|?*]', '', filename)
 
 def download(format, videos):
     if format == "mp4":
@@ -10,7 +14,7 @@ def download(format, videos):
     elif format == "mp3":
         folder_name = "audios"
     else:
-        print("Invalid format. Choose mp4 or mp3")
+        print("Invalid format. Choose 'mp4' or 'mp3'")
         return
 
     if not os.path.exists(folder_name):
@@ -19,15 +23,16 @@ def download(format, videos):
     for video in videos:
         yt = YouTube(video)
         title = yt.title
+        filename = sanitize_filename(title)
 
         if format == "mp4":
             stream = yt.streams.get_highest_resolution()
-            stream.download(output_path=folder_name, filename=title + ".mp4")
-            print(f"Downloaded {title} as video.")
+            stream.download(output_path=folder_name, filename=filename + ".mp4")
+            print(f"Downloaded {filename} as video.")
 
         elif format == "mp3":
             stream = yt.streams.filter(only_audio=True).first()
-            stream.download(output_path=folder_name, filename=title + ".mp3")
-            print(f"Downloaded {title} as audio.")
+            stream.download(output_path=folder_name, filename=filename + ".mp3")
+            print(f"Downloaded {filename} as audio.")
 
 download(FORMAT, VIDEOS)
